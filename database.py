600b68e3a9a242ddf4f2252a687a7fe0fc960b8d
Tiara Jaque <tiarajaque00@gmail.com>
2026-09-16 15:39:19 -0300

    Primer commit

---
+++ database.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# CONFIGURACIÓN DE LA BASE DE DATOS
+
+DATABASE_URL = "mysql+pymysql://root:TU_CONTRASEÑA@localhost/dragon_hambriento"
+
+# Crear conexión
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+# Crear sesiones
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+# Clase base para nuestros modelos
+Base = declarative_base()
+
+
+# FUNCIÓN PARA OBTENER UNA SESIÓN
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
